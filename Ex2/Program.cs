8a88402ff3ac7a992b60732adb03619e895bc6fc
Alex <trainofbird@gmail.com>
2023-05-22 23:30:55 +0300

    задачи решены, кроме 5

---
+++ Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу замена элементов массива: положительные 
+// элементы замените на соответствующие отрицательные, и наоборот.
+
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int[] GetArray(int size, int minValye, int maxValye)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(minValye, maxValye + 1);
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+void NegativeNumArr(int[] userArray)
+{
+    int arrSize = userArray.Length;
+    for (int el = 0; el < arrSize; el++)
+    {
+        userArray[el] *= -1;
+        Console.Write($"{userArray[el]} ");
+    }
+}
+
+int[] array = GetArray(4, -9, 9);
+Console.WriteLine();
+NegativeNumArr(array);
---
+++ Ex3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует 
+// ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// использовать преобразование чисел по модулю (интересует только значение числа)
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        //Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+void CheckNumArr(int[] arr, int num)
+{
+    bool check = false;
+    num = Math.Abs(num);
+    foreach (int el in arr)
+    {
+        if (el == num)
+        {
+            Console.WriteLine($"{num}; массив [ {String.Join(" ", arr)} ] -> да");
+            check = true;
+            break;
+        }
+    }
+    if (check == false)
+    {
+        Console.WriteLine($"{num}; массив [ {String.Join(" ", arr)} ] -> нет");
+    }
+}
+
+int[] array = GetArray(10, -10, 10);
+Console.WriteLine();
+CheckNumArr(array, -4);
